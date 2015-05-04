3825ceaf64f638aa6e23411dc8178073d5db481f
Oliver Cox <oliver@schmine.com>
2015-05-04 16:41:56 -0400

    Added config for python service

--- config/secrets.js
+++ config/secrets.js
@@ -30,6 +30,7 @@ module.exports = {
 
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
 
+  python_service_url: process.env.SESSION_SECRET || 'http://teamav-python.mybluemix.net'
   mailgun: {
     user: process.env.MAILGUN_USER || 'postmaster@sandbox697fcddc09814c6b83718b9fd5d4e5dc.mailgun.org',
     password: process.env.MAILGUN_PASSWORD || '29eldds1uri6'
