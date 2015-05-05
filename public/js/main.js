11570edd881695290621f86e45c50413f6d773b5
Oliver Cox <oliver@schmine.com>
2015-05-05 15:45:07 -0400

    Added geo location Added tweet to output json file

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,31 @@
+var geocoder;
 $(document).ready(function() {
-
+  geocoder = new google.maps.Geocoder();
   // Place JavaScript code here...
+  if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(codeLatLng);
+      } else {
+          x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+
 
 });
+
+function codeLatLng(position) {
+  var lat = parseFloat(position.coords.latitude);
+  var lng = parseFloat(position.coords.longitude);
+  var latlng = new google.maps.LatLng(lat, lng);
+  geocoder.geocode({'latLng': latlng}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      if (results[1]) {
+        console.log(results);
+        var addressLabel = document.getElementById("userAddress");
+        addressLabel.innerText = results[0].formatted_address;
+      } else {
+        alert('No results found');
+      }
+    } else {
+      alert('Geocoder failed due to: ' + status);
+    }
+  });
+}
